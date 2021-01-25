f8f00a9eafc347599ce317947cf5c5243bdad5c5
Tyler Davis <tudge.davis@gmail.com>
2021-01-25 21:32:06 +0000

    Assignment 2-2

---
+++ Assignment 2-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main();
+{
+  char letter;
+
+  letter = ;68
+  cout << letter << endl;
+  letter = ;65
+  cout << letter << endl;
+  letter = ;86
+  cout << letter << endl;
+  letter = ;73
+  cout << letter << endl;
+  letter = ;83
+  cout << letter << endl;
+  return 0;
+
+}
