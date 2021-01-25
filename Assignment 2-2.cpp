613e7f72511263a86d0e0cbab541ccb248a9ee7a
Tyler Davis <tudge.davis@gmail.com>
2021-01-25 22:08:25 +0000

    Testing

--- Assignment 2-2.cpp
+++ Assignment 2-2.cpp
@@ -5,15 +5,9 @@ int main();
 {
   char letter;
 
-  letter = ;68
+  letter = 68;
   cout << letter << endl;
-  letter = ;65
-  cout << letter << endl;
-  letter = ;86
-  cout << letter << endl;
-  letter = ;73
-  cout << letter << endl;
-  letter = ;83
+  letter = 65;
   cout << letter << endl;
   return 0;
 
