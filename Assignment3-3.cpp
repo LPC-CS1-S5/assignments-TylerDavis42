5e95fb997617795884c3af410026e4d8f5f9ddf4
Tyler Davis <tudge.davis@gmail.com>
2021-02-09 21:41:41 +0000

    Assignment 3-3

---
+++ Assignment3-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+double F;
+double C;
+
+cout << "Enter Celsius Temperature" << endl;
+cin >> C;
+
+F = 1.8*C + 32;
+
+cout << C << "C" << " is " <<  F << "F" << endl;
+
+}
