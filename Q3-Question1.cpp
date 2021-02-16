0847ddfe4e4bbccce0c6cce74ff0a7547e6b433b
Tyler Davis <tudge.davis@gmail.com>
2021-02-16 19:32:36 +0000

    Quiz 3-2

---
+++ Q3-Question1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+  
+const int Max_Value = 1000;
+const int Min_Value = 0;
+
+int Rnum1;
+int Rnum2;
+int Rnum3;
+
+unsigned seed = time(0);
+srand(seed);
+
+Rnum1 = (rand() % Max_Value - Min_Value + 1) + Min_Value;
+Rnum2 = (rand() % Max_Value - Min_Value + 1) + Min_Value;
+Rnum3 = (rand() % Max_Value - Min_Value + 1) + Min_Value;
+
+int SUM;
+SUM = Rnum1 + Rnum2 + Rnum3;
+
+int AVG;
+AVG = SUM/3;
+
+cout << Rnum1 << endl;
+cout << Rnum2 << endl;
+cout << Rnum3 << endl;
+cout << "Sum: " << SUM << endl;
+cout << setprecision(5) << "Average: " << AVG << endl;
+
+}
